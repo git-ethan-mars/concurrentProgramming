f4d0f339074909b00911be30172990a78142829a
git-ethan-mars <73284049+git-ethan-mars@users.noreply.github.com>
2022-11-09 19:03:35 +0500

    Deleted useless lock

--- Homework1/Homework1/Program.cs
+++ Homework1/Homework1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
+using System.Text;
 
-namespace ConsoleApp1
+namespace Homework1
 {
     class Program
     {
@@ -8,9 +9,7 @@ namespace ConsoleApp1
         {
             var process = Process.GetCurrentProcess();
             var data = new List<Tuple<int, long>>();
-#pragma warning disable CA1416
-            process.ProcessorAffinity = (IntPtr)32768;
-#pragma warning restore CA1416
+            process.ProcessorAffinity = (IntPtr) Math.Pow(2, Environment.ProcessorCount - 1);
             process.PriorityClass = ProcessPriorityClass.RealTime;
             var stopwatch = new Stopwatch();
             stopwatch.Start();
@@ -18,28 +17,20 @@ namespace ConsoleApp1
             {
                 for (var i = 0; i < 100000; i++)
                 {
-                    lock (data)
-                    {
-                        // ReSharper disable once NotAccessedVariable
-                        var str = "a";
-                        for (int j = 0; j < 100; j++)
-                            str += "a";
-                        data.Add(Tuple.Create(1, stopwatch.ElapsedMilliseconds));
-                    }
+                    var str = new StringBuilder("a");
+                    for (var j = 0; j < 100; j++)
+                        str.Append('a');
+                    data.Add(Tuple.Create(1, stopwatch.ElapsedMilliseconds));
                 }
             });
             var thread2 = new Thread(() =>
             {
                 for (var i = 0; i < 100000; i++)
                 {
-                    // ReSharper disable once NotAccessedVariable
-                    var str = "a";
-                    lock (data)
-                    {
-                        for (int j = 0; j < 100; j++)
-                            str += "a";
-                        data.Add(Tuple.Create(2, stopwatch.ElapsedMilliseconds));
-                    }
+                    var str = new StringBuilder("a");
+                    for (var j = 0; j < 100; j++)
+                        str.Append('a');
+                    data.Add(Tuple.Create(2, stopwatch.ElapsedMilliseconds));
                 }
             });
             thread.Start();
@@ -50,13 +41,17 @@ namespace ConsoleApp1
             long sum = 0;
             long temp = 0;
             var threadNum = 1;
-            foreach (var item in data.Where(item => item.Item1 != threadNum))
+            foreach (var item in data)
             {
-                threadNum = item.Item1;
-                count++;
-                sum += (item.Item2 - temp);
-                temp = item.Item2;
+                if (item.Item1 != threadNum)
+                {
+                    threadNum = item.Item1;
+                    count++;
+                    sum += (item.Item2 - temp);
+                    temp = item.Item2;
+                }
             }
+
             // ReSharper disable once IntDivisionByZero
             Console.WriteLine(sum / count);
         }
