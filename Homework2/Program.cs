f9befed14a2d9abcbb5ccc73deaf78e7a0598b47
git-ethan-mars <73284049+git-ethan-mars@users.noreply.github.com>
2022-11-12 04:20:06 +0500

    Homework 2

---
+++ Homework2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Homework2;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var multiLock = new MultiLock();
+        var testVariable = new[]{"1","4"};
+        var testVariable2 = new[]{"1","2","3"};
+        var thread1 = new Thread(() =>
+        {
+            using (multiLock.AcquireLock(testVariable))
+            {
+                Thread.Sleep(1000);
+            }
+            
+        });
+        var thread2 = new Thread(() =>
+        {
+            Thread.Sleep(500);
+            using(multiLock.AcquireLock(testVariable2))
+            {
+                Thread.Sleep(500);
+            }
+            
+        });
+        thread1.Start();
+        thread2.Start();
+    }
+}
+
