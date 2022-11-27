a41c00deecf5c0bdb3938d4fd5dcbb84f2290105
git-ethan-mars <73284049+git-ethan-mars@users.noreply.github.com>
2022-11-27 14:18:04 +0500

    Fixed lock

---
+++ Homework2/Disposable.cs
@@ -0,0 +1,17 @@
+﻿namespace Homework2;
+
+public class Disposable : IDisposable
+{
+    private object[] _currentKeys;
+    public Disposable(object[] keys)
+    {
+        _currentKeys = keys;
+    }
+    public void Dispose()
+    {
+        foreach (var key in _currentKeys)
+        {
+            Monitor.Exit(key);
+        }
+    }
+}
