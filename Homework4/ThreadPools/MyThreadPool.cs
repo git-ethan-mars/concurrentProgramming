5b53ff7438a5999c2bc834aaa9494739c3dd5aba
git-ethan-mars <73284049+git-ethan-mars@users.noreply.github.com>
2022-12-13 19:45:10 +0500

    Homework 4

---
+++ Homework4/ThreadPools/MyThreadPool.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+namespace CustomThreadPool
+{
+    public class MyThreadPool : IThreadPool
+    {
+        private long _tasksProcessedCount;
+        private readonly Queue<Action> _queue;
+        private readonly int _maxThreadsCount;
+        private readonly Thread[] _threads;
+
+
+        public MyThreadPool()
+        {
+            _queue = new Queue<Action>();
+            _maxThreadsCount = Environment.ProcessorCount * 2;
+            _threads = new Thread[_maxThreadsCount];
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            for (var i = 0; i < _maxThreadsCount; i++)
+            {
+                var thread = new Thread(WorkingThread)
+                {
+                    IsBackground = true,
+                };
+                _threads[i] = thread;
+                thread.Start();
+            }
+        }
+
+        private void WorkingThread()
+        {
+            while (true)
+            {
+                lock (_queue)
+                {
+                    if (_queue.TryDequeue(out var action))
+                    {
+                        action();
+                        Interlocked.Increment(ref _tasksProcessedCount);
+                        
+                    }
+                    else
+                    {
+                        Monitor.Wait(_queue);
+                    }
+                }
+            }
+        }
+
+        public void EnqueueAction(Action action)
+        {
+            lock (_queue)
+            {
+                _queue.Enqueue(action);
+                Monitor.Pulse(_queue);
+            }
+        }
+
+        public long GetTasksProcessedCount()
+        {
+            return _tasksProcessedCount;
+        }
+    }
+}
